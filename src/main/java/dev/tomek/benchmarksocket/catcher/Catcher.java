e59319f6ca311cca29812a53881e7cd34277a030
Tomek Szmytka <tszmytka@gmail.com>
2020-02-24 20:16:42 +0100

    Reduce boilerplate code, have pitcher and cacher as applications

--- src/main/java/dev/tomek/benchmarksocket/catcher/Catcher.java
+++ src/main/java/dev/tomek/benchmarksocket/catcher/Catcher.java
@@ -3,17 +3,22 @@ package dev.tomek.benchmarksocket.catcher;
 import dev.tomek.benchmarksocket.catcher.transport.CatchTransport;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
-@Component
+@SpringBootApplication
 @RequiredArgsConstructor
 @Log
 public class Catcher {
     private final Collection<CatchTransport> catchTransports;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Catcher.class, args);
+    }
 
     @EventListener(ApplicationReadyEvent.class)
     public void start() {
--- src/main/java/dev/tomek/benchmarksocket/pitcher/Pitcher.java
+++ src/main/java/dev/tomek/benchmarksocket/pitcher/Pitcher.java
@@ -2,17 +2,21 @@ package dev.tomek.benchmarksocket.pitcher;
 
 import dev.tomek.benchmarksocket.pitcher.transport.PitchTransport;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
-@Component
 @RequiredArgsConstructor
+@SpringBootApplication
 public class Pitcher {
-
     private final Collection<PitchTransport> pitchTransports;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pitcher.class);
+    }
 
     @EventListener(ApplicationReadyEvent.class)
     public void start() {
