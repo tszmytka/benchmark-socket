1f6c94311e9a4a7d1635489280b3ae0ee2d139e5
Tomek Szmytka <tszmytka@gmail.com>
2020-03-08 13:45:51 +0100

    Allow naive test for quickly running while developing

---
+++ src/test/java/dev/tomek/benchmarksocket/catcher/CatcherTest.java
@@ -0,0 +1,15 @@
+package dev.tomek.benchmarksocket.catcher;
+
+import dev.tomek.benchmarksocket.config.CommonConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(CommonConfig.PARAM_TRANSPORT + "=" + CommonConfig.TRANSPORT_SOCKET_PLAIN)
+class CatcherTest {
+
+    @Test
+    void canLoadContext() {
+        // this will only hang at the moment
+    }
+
+}
